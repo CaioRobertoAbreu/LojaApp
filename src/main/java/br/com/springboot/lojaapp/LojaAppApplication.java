511dcc8c3a604acb00600fd15e13953cfb1a0d21
Caio Abreu <caio3015@hotmail.com>
2020-09-11 15:59:11 -0300

    Projeto iniciado.

---
+++ src/main/java/br/com/springboot/lojaapp/LojaAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.springboot.lojaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/springboot/lojaapp/LojaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springboot.lojaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
